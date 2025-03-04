6051c3199538dfcb9faab876bc372ceb9aa5b445
muhkeun <muhkeun@naver.com>
2025-03-04 15:12:33 +0900

    Project Init

---
+++ src/test/java/com/muhkeun/productmanagerapi/ProductManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhkeun.productmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
